d99958ea1250628750b16212f4b9e0c0213160b2
Hamo_Minasyan <hamo.minasyan93@gmail.com>
2021-12-16 16:47:38 +0400

    bug fix

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
 export const ERR = {
-    initialValues: (method, dataKey) => throw new Error(`[${dataKey}] key is not defined in initial values! (method: ${method})`)
+    initialValues: (method, dataKey) => console.error(`[${dataKey}] key is not defined in initial values! (method: ${method})`)
 };
 
 export const buildUniqueKeyName = uniqueKey => `subscriberKey-${uniqueKey}`;
